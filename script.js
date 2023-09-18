4f1e4593cb95be23d80a770fbcc0a45bd644907e
Sandhiya R <105971993+sandhiya-13@users.noreply.github.com>
2023-09-18 10:47:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
+        alert("Kindly Enter Task Name!!!!")
     }
 
     else{
