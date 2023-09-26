84c575a361c94b5c93faabde81e55764e88c8bff
Sandhiya R <105971993+sandhiya-13@users.noreply.github.com>
2023-09-26 23:06:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
-        alert("Kindly Enter Task Name!!!!")
+        alert("Please Enter a Task")
     }
-
     else{
-        document.querySelector('#tasks').innerHTML += '
+        document.querySelector('#tasks').innerHTML += `
             <div class="task">
                 <span id="taskname">
                     ${document.querySelector('#newtask input').value}
@@ -13,7 +12,7 @@ document.querySelector('#push').onclick = function(){
                     <i class="far fa-trash-alt"></i>
                 </button>
             </div>
-        ';
+        `;
 
         var current_tasks = document.querySelectorAll(".delete");
         for(var i=0; i<current_tasks.length; i++){
@@ -21,5 +20,14 @@ document.querySelector('#push').onclick = function(){
                 this.parentNode.remove();
             }
         }
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
     }
 }
