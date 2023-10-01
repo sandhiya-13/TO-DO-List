01a1bc96b41bbc12e54d2e6f68ea6748b832dff5
Sandhiya R <105971993+sandhiya-13@users.noreply.github.com>
2023-10-01 13:52:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
         alert("Please Enter a Task")
     }
+
     else{
         document.querySelector('#tasks').innerHTML += `
             <div class="task">
@@ -20,14 +21,5 @@ document.querySelector('#push').onclick = function(){
                 this.parentNode.remove();
             }
         }
-
-        var tasks = document.querySelectorAll(".task");
-        for(var i=0; i<tasks.length; i++){
-            tasks[i].onclick = function(){
-                this.classList.toggle('completed');
-            }
-        }
-
-        document.querySelector("#newtask input").value = "";
     }
 }
